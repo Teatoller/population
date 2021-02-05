cb20dbb63e59dc8bd6e8407bcce489af59de476c
Teatoller <steven.otieno1@gmail.com>
2021-02-05 10:33:46 +0300

    (csv file): Implement csvreader
    - populate array from csv file
    - add csv file
    - add test folder
    
    [Starts #002]

---
+++ src/Population/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Population
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"Popular Indicators/91aff299-f948-4300-b856-816274042579_Data.csv";
+            CsvReader reader = new CsvReader(filePath);
+
+            Country[] countries = reader.ReadFirstNCountries(10);
+
+            foreach (Country country in countries)
+            {
+                Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
+            }
+        }
+    }
+}
